da40d40c2c6677cecfb380db0616460e58bbb5f5
Sergei Shubenkov <sergunwe152@gmail.com>
2024-08-19 22:28:33 +0300

    Improve camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,6 +5,8 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private Tilemap tilemap;
     [SerializeField] private Camera gameCamera;
+    [SerializeField] private Vector3 centerOffset = Vector3.zero;
+    [SerializeField] private float sizeOffset = 0;
 
     private void Awake()
     {
@@ -16,15 +18,15 @@ public class CameraController : MonoBehaviour
     void SetCameraCenter()
     {
         Vector3 tilemapCenter = tilemap.cellBounds.center;
-        gameCamera.transform.position = new Vector3(tilemapCenter.x, tilemapCenter.y, gameCamera.transform.position.z);
+        gameCamera.transform.position = new Vector3(tilemapCenter.x, tilemapCenter.y, gameCamera.transform.position.z) + centerOffset;
     }
 
     private void SetCameraSize()
     {
         Bounds tilemapBounds = tilemap.localBounds;
         float aspectRatio = Screen.width / (float)Screen.height;
-        float verticalSize = tilemapBounds.size.y / 2f;
-        float horizontalSize = tilemapBounds.size.x / 2f / aspectRatio;
-        gameCamera.orthographicSize = Mathf.Max(verticalSize, horizontalSize);
+        float verticalSize = Mathf.Max(tilemapBounds.size.y / 2f, tilemapBounds.size.x / 2f);
+        float horizontalSize = Mathf.Max(tilemapBounds.size.x / 2f / aspectRatio, tilemapBounds.size.y / 2f / aspectRatio);
+        gameCamera.orthographicSize = Mathf.Max(verticalSize, horizontalSize) + sizeOffset;
     }
 }
